66329a049300a3c1bf838bf5fabca0fd068fe09e
leonard3030 <leonardremy3030@gmail.com>
2020-10-23 19:03:08 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Omega | Home</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+    <div class="nav-container">
+        <nav>
+            <ul>
+                <a href="" class="nav-brand">Omega</a>
+                <div class="middle">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Works</a></li>
+                </div>
+                <li><a href="" class="talk">Let's Talk</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="port-section">
+        <div class="port-container">
+            <div class="img-widget">
+                <img src="assets/img/1.jpg">
+            </div>
+            <div class="info-widget">
+                <h4 class="salutation">Hello Everybody, i am</h4>
+                <h1 class="my-name">TUMUSHIME Leonard</h1>
+                <h1 class="my-grade">Full Stack Web Developer</h1>
+                <p class="my-desc">My Name is TUMUSHIME Leonard Alias Omega, I am a Hard-working Full Stack Web Developer, with a flair for creating elegant solutions in the least amount of time. I'm passionate about software architecture and cloud computing. </p>
+                <p class="my-desc">
+                    <span class="fa fa-phone"></span>&nbsp;&nbsp; (+250) 789-178-645&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="fa fa-envelope"></span>&nbsp;&nbsp; leonardremy3030@gmail.com
+                </p>
+                <a href="#" class="my-works">View Works</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
